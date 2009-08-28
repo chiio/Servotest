1ae9efc6f1e38a633345314758f08a71893ef38f
Fabiano <fabiano@HPFabiano.(none)>
2009-08-28 14:10:22 +0200

    Automatic procedure to measure the servo's performance and save all plot in RPlots.pdf

--- servo_test.rb
+++ servo_test.rb
@@ -5,7 +5,7 @@ running = true
 
 begin
 	port_file = "/dev/ttyUSB0"
-	sp = SerialPort.new(port_file, 9600, 8, 1, SerialPort::NONE)
+	sp = SerialPort.new(port_file, 57600, 8, 1, SerialPort::NONE)
 rescue
 	warn "ERROR: No device connected."
 	exit(0)
@@ -13,51 +13,79 @@ end
 
 iang = 20
 fang = 100
-time = 5000
+time = [5000,3500,2500,1750,1000,500,250,100,50,10]
+omegaMean = Array.new()
+time.each_with_index {|t,k| omegaMean[k] = (fang-iang)/t.to_f}
 
+sp.puts "r"
 sp.puts "#{iang}i"
 sp.puts "#{fang}f"
-sp.puts "#{time}t"
-sp.puts "r"
 
+data = Array.new()
+
+time.each_with_index do |period,k|
+
+data << {:time => Array.new(),
+		:theoric_angle => Array.new(),
+		:measured_angle => Array.new()}
+		
+sp.puts "#{period}t"
+
+sleep 0.1
 sp.puts "s"
 sleep 0.1
 
-data = {:time => Array.new(),
-		:theoric_angle => Array.new(),
-		:measured_angle => Array.new()}
-
 while running
+	sleep 0.05
 	line = sp.gets
 	if line != nil
 		line = line.split
-		data[:time] << line[0].to_f
-		data[:theoric_angle] << line[1].to_f
-		data[:measured_angle] << line[2].to_f/10.0
+		data[k][:time] << line[0].to_f
+		data[k][:theoric_angle] << line[1].to_f
+		data[k][:measured_angle] << line[2].to_f/10.0
 	end	
-	running = false if line == nil
-	sleep 0.05	
+	running = false if line == nil	
 end
 
-data.each {|d| d.delete_at(0)}
+data[k].each {|d| d.delete_at(0)}
 
 sp.puts "r"
+#puts "Press ENTER to continue"
+#gets
+sleep 1
+running = true
+end
+
+graph = true
+
+maxDerivFunction = ["maxDeriv <- function (time,angle) {\n",
+					"dc = c(1:length(time)-2)\n",
+					"for (i in 1:length(time)-2) {\n",
+					"dc[i] = (angle[i+2]-angle[i])/(time[i+2]-time[i])}\n",
+					"max(dc) }" ]
 
 File.open("servotest.r", "w") do |f| 
 	f.puts "rm(list=ls(all=TRUE));"
-	f.puts "split.screen(c(2,1));"
-	f.puts "time <- c(#{data[:time].join(",")});"
-	f.puts "theoric_angle <- c(#{data[:theoric_angle].join(",")});"
-	f.puts "measured_angle <- c(#{data[:measured_angle].join(",")});"
-	f.puts "screen(1);"	
-	f.puts "plot(time,measured_angle, main=\"#{iang} -> #{fang} in #{time} ms\");"
-	f.puts "screen(2);"
-	f.puts "plot(theoric_angle,measured_angle, main=\"#{iang} -> #{fang} in #{time} ms\");"
-	f.puts "df <- data.frame(time, R = measured_angle);"
-	f.puts "df.lm <- lm(R ~ time^2, data = df);"
-	f.puts "coefficients(df.lm)"
+	f.puts "omegaMean = c(#{omegaMean.join(",")})"
+	f.puts "omegaMax = c(1:#{omegaMean.length})"
+	f.puts maxDerivFunction
+	omegaMean.each_with_index do |w,i|
+		f.puts "time <- c(#{data[i][:time].join(",")})"
+		f.puts "theoric_angle <- c(#{data[i][:theoric_angle].join(",")})"
+		f.puts "measured_angle <- c(#{data[i][:measured_angle].join(",")})"
+		if graph
+			f.puts "split.screen(c(2,1), erase = TRUE)"
+			f.puts "screen(1)"	
+			f.puts "plot(time,measured_angle, main=\"#{iang} -> #{fang} in #{time[i]} ms\")"
+			f.puts "screen(2)"
+			f.puts "plot(theoric_angle,measured_angle)"
+			f.puts "close.screen(all = TRUE)"
+		end
+		f.puts "omegaMax[#{i+1}] = maxDeriv(time,measured_angle)"
+	end
+	f.puts "plot(omegaMean,omegaMax,\"b\")"
+	f.puts "close.screen(all = TRUE)"
 end
 
-output = exec "R --vanilla -q < servotest.r"
+exec "R --vanilla -q < servotest.r"
 
-puts output.inspect
